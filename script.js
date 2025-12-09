4a6a196f990a895efd6e43441caaece389a6f11a
plabondas001 <plabondas001@gmail.com>
2025-12-09 15:32:36 +0600

    add button text color

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ document.getElementById('btn-click').addEventListener('click', function () {
     document.getElementById('name-title').innerText = 'My name is Plabon Das.'
     document.getElementById('name-title').style.color = 'blue'
     document.getElementById('btn-click').style.backgroundColor = 'green'
+    document.getElementById('btn-click').style.color = 'white'
 
 })

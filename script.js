fffb8f93843fb4b13d10c1f5d33a1488519d051c
plabondas001 <plabondas001@gmail.com>
2025-12-09 15:30:09 +0600

    add innertext

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('btn-click').addEventListener('click', function () {
     document.body.style.backgroundColor = 'orange'
+    document.getElementById('name-title').innerText = 'My name is Plabon Das.'
 
 })

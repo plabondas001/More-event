7f7f8621299e64d7d81c1c64e5fefdd791cad0f6
plabondas001 <plabondas001@gmail.com>
2025-12-09 15:30:57 +0600

    change innertext color

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById('btn-click').addEventListener('click', function () {
     document.body.style.backgroundColor = 'orange'
     document.getElementById('name-title').innerText = 'My name is Plabon Das.'
+    document.getElementById('name-title').style.color = 'blue'
 
 })
